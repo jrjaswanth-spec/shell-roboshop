c7e24dee6d6d8af27c8e1e887b04a23106adaf33
jaswanth <jaswanthr7@jaswanths-MacBook-Air.local>
2026-02-20 22:38:45 +0530

    s

---
+++ redis.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d. -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+START_TIME=$(date +%s)
+mkdir -p $LOGS_FOLDER
+echo "script started executed at: $( "date" )" &>>$LOG_FILE
+
+if [ $USERID -ne 0 ]
+then
+     echo "ERROR: please run this script with root privelage"
+     exit 1
+fi
+
+
+VALIDATE(){
+        if [ $1 -ne 0 ] 
+    then
+         echo -e "$2...$R FAILURE $N"
+         exit 1
+    else
+         echo -e "$2....$G SUCCESS $N"
+    fi     
+}
+
+dnf module disable redis -y &>>$LOG_FILE
+VALIDATE $? "disabling default redis"
+dnf module enable redis:7 -y &>>$LOG_FILE
+VALIDATE $? "enabling redis:7"
+dnf install redis -y &>>$LOG_FILE
+VALIDATE $? "installing redis"
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "allowing remote connections to redis"
+
+systemctl enable redis &>>$LOG_FILE
+VALIDATE $? "enabling redis"
+
+systemctl start redis &>>$LOG_FILE
+VALIDATE $? "starting redis"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+echo -e "script executed in: $Y $TOTAL_TIME seconds $N"
